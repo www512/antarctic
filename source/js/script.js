ac2c806a3a1967f3cc9bd48dbe74849a00d8c0ed
www512 <www512@yandex.ru>
2020-02-07 15:02:01 +0300

    Antarctic

---
+++ source/js/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+let header = document.querySelector('.header');
+let headerToggle = header.querySelector('.header__toggle-on');
+let logoImage = header.querySelector('.logo__image-on');
+let headerNav = header.querySelector('.header-navigation');
+
+function offButton() {
+  header.classList.remove('header__on');
+  headerToggle.classList.remove('header__toggle-on');
+  headerToggle.classList.add('header-toggle');
+  logoImage.classList.add('logo-image');
+  logoImage.classList.remove('logo__image-on');
+  headerNav.classList.add('navigation-off');
+}
+
+function onButton() {
+  header.classList.add('header__on');
+  headerToggle.classList.add('header__toggle-on');
+  headerToggle.classList.remove('header-toggle');
+  logoImage.classList.add('logo__image-on');
+  logoImage.classList.remove('logo-image');
+  headerNav.classList.remove('navigation-off');
+}
+
+offButton();
+
+headerToggle.addEventListener('click', function () {
+  if (header.classList.contains('header__on')) {
+    offButton();
+  } else {
+    onButton();
+  }
+});
